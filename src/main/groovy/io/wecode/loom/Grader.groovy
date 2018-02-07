c2fae48c6fde5bd02c3bbf25d20458816eeb8740
Lucas Videla <videla.lucas@gmail.com>
2018-02-07 11:53:57 -0300

    Se remueve la base de datos, y la conexión con la API de GitHub

--- src/main/groovy/io/wecode/loom/Grader.groovy
+++ src/main/groovy/io/wecode/loom/Grader.groovy
@@ -1,89 +1,44 @@
 package io.wecode.loom
 
 import groovy.io.FileType
-import groovy.sql.Sql
 import groovy.util.logging.Log4j
 import groovyx.net.http.ContentType
 import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.Method
 import org.apache.commons.io.FileUtils
 import org.apache.http.conn.HttpHostConnectException
-
-// 0. en un while eterno, o ejecutandose cada X minutos
-
-// por cada tarea en el json de tareas
+import groovy.json.JsonSlurper
+import groovy.json.StringEscapeUtils
 
 @Log4j
 class GraderScript {
 
     def execute() {
 
-        // esto corta la ejecución si ya hay un grader funcionando en el sistema.
-        // quitar si se paraleliza
+        // esto corta la ejecución si ya hay un grader funcionando en el sistema
         prevenirDobleEjecucion()
-
         PropertiesHolder.instance.load()
 
+        def pause = 0;
         while (true) {
 
-            def sql = Sql.newInstance('jdbc:sqlite:loom.sqlite', 'org.sqlite.JDBC')
+            log.info "Buscando repositorios pendientes"
+            def pendingRepos = new JsonSlurper().parseText("${PropertiesHolder.instance.properties.DASHBOARD_HOST}/api/v1/repos/pending".toURL().
+            getText(requestProperties: ['x-api-key': PropertiesHolder.instance.properties.GRADER_TOKEN]))
 
-            sql.eachRow("select * from assignments") { repoInDB ->
+            if (pendingRepos.size() == 0) {
+                pause = Math.min(5, (++pause))
+                log.info "No se encontraron repos pendientes. En pausa por ${pause} minuto(s)... zZzZ"
+                sleep(pause * 60 * 1000)
+            } else {
+                pause = 0
+                def fork = pendingRepos[0]
+                def repo = fork.parent
 
-                log.info "procesando repo: ${repoInDB.full_name}"
-                def repo = GithubAPI.info(repoInDB.full_name)
+                log.info "Se corregirá el repo ${fork.git_url}."
 
-                GithubAPI.forks(repoInDB.full_name).each { fork ->
-                    log.info "procesando fork: ${fork.full_name}"
-
-                    def forksData = sql.dataSet("forks")
-                    if (fork.size < 512) {
-
-                        // si no existe
-                        if (sql.rows("select * from forks where full_name = ?", [fork.full_name]).size == 0) {
-                            log.info "dando de alta el fork"
-                            forksData.add(
-                                    assignment_full_name: repoInDB.full_name,
-                                    user: fork.owner.login,
-                                    full_name: fork.full_name,
-                                    repo_url: fork.git_url,
-                                    updated_at: new Date().parse("YYYY-MM-dd'T'hh:mm:ss'Z'", fork.updated_at),
-                                    tested_at: null
-                            )
-                        } else {
-                            log.info "se vuelve a corregir fork existente"
-                            sql.executeUpdate("update forks set updated_at = ? where full_name = ?",
-                                    [
-                                            new Date().parse("YYYY-MM-dd'T'hh:mm:ss'Z'", fork.updated_at),
-                                            fork.full_name,
-                                    ])
-                        }
-
-                        // si es mas nuevo, o si tiene una actualizacion, correr tests
-                        def forkInDB = sql.firstRow("select * from forks where full_name = ?", [fork.full_name])
-                        // TODO: quitar este "true", claramente :)
-                        if (true || !forkInDB.tested_at || forkInDB.updated_at > forkInDB.tested_at) {
-                            log.info "ejecutando tests..."
-
-                            test(repoInDB, forkInDB, repo, fork)
-
-                            sql.executeUpdate("update forks set tested_at = ? where full_name = ?",
-                                    [new Date(), fork.full_name])
-                            log.info "ejecucion terminada en ${new Date()}"
-                        } else {
-                            log.info "no es necesario correr los tests"
-                        }
-
-                    } else {
-                        log.info "el fork [${fork.full_name}] excede el tamaño maximo permitido"
-                    }
-                }
-
+                test(repo, fork)
             }
-
-            def pause = 10_000
-            log.info "en pausa por ${pause / 1_000} segundos... zZzZ"
-            sleep(pause)
         }
     }
 
@@ -101,29 +56,37 @@ class GraderScript {
         }
     }
 
-    def test(def repoInDB, def forkInDB, def repoo, def forko) {
+    def test(def repo, def fork) {
 
         def deletables = []
         // TODO: poner todos los clones en un directorio aparte, asi no quedan sueltos en el proyecto
 
-        def canonicalCloneDir = "${repoInDB.user}-${repoInDB.repo}"
-        def canonicalClone = "git clone --depth 1 -b resolucion ${repoInDB.tests_url} ${canonicalCloneDir}".execute()
+        log.info "Clonando repositorio ${repo.git_url}"
+        def canonicalCloneDir = "${repo.user}-${repo.name}"
+        def canonicalClone = "git clone --depth 1 -b resolucion ${repo.git_url} ${canonicalCloneDir}".execute()
         canonicalClone.waitFor()
-        deletables << new File("./${repoInDB.user}-${repoInDB.repo}")
+        log.info "Repo clonado correctamente"
+        deletables << new File("./${canonicalCloneDir}")
 
         // clonamos
-        def solutionCloneDir = "${forkInDB.user}-${repoInDB.repo}-${new Date().time}"
-        def solutionClone = "git clone --depth 1 ${forkInDB.repo_url} ${solutionCloneDir}".execute()
+        log.info "Clonando repositorio ${fork.git_url}"
+        def solutionCloneDir = "${fork.user}-${repo.name}-${new Date().time}"
+        def solutionClone = "git clone --depth 1 ${fork.git_url} ${solutionCloneDir}".execute()
         solutionClone.waitFor()
         deletables << new File("./${solutionCloneDir}")
 
+        def sha = fork['sha'] = ['git', '--git-dir', "./${solutionCloneDir}/.git", 'rev-parse', 'HEAD'].execute().text
+        log.info "Se corregirá el commit ${sha}"
+
+
         if (solutionClone.exitValue() == 1) {
             log.info 'No se ha podido hacer clone de la solución'
-            def report = armarReporteFallido(repoo, forko, new Score(value: 0, total: 10), 'No se ha podido hacer clone de la solución')
-            enviarReporte(repoInDB, report)
+            def report = armarReporteFallido(fork, new Score(value: 0, total: 10), 'No se ha podido hacer clone de la solución')
+            enviarReporte(fork, report)
             borrarArchivos(deletables)
             return
         }
+        log.info "Repo clonado correctamente"
 
         // mergeamos con los tests
         try {
@@ -132,116 +95,109 @@ class GraderScript {
             FileUtils.copyDirectory(new File("./${solutionCloneDir}/src/main/java/"), new File("${canonicalCloneDir}/src/main/java/"))
         } catch (FileNotFoundException fe) {
             log.info 'Se envió un repo no válido'
-            def report = armarReporteFallido(repoo, forko, new Score(value: 0, total: 10), 'Se envió un repo no válido')
-            enviarReporte(repoInDB, report)
+            def report = armarReporteFallido(fork, new Score(value: 0, total: 10), 'Se envió un repo no válido')
+            enviarReporte(fork, report)
             borrarArchivos(deletables)
             return
         }
+        log.info "Se reemplazó el código del fork dentro del repo"
 
         // compilamos
-        def compileMerge = "gradle -p ${canonicalCloneDir} compileJava".execute()
+        def compileMerge = ['docker', 'run', '--rm', '-v', (System.getProperty("user.dir") + '/' + canonicalCloneDir + ':/home/gradle/project'), '-w', '/home/gradle/project', 'gradle:alpine', 'gradle', 'compileJava'].execute()
         compileMerge.waitFor()
 
-        // No compila el código
+        // Si no compila el código
         if (compileMerge.exitValue() == 1) {
-            def report = armarReporteFallido(repoo, forko, new Score(value: 0, total: 10), compileMerge.in.text)
-            enviarReporte(repoInDB, report)
+            def report = armarReporteFallido(fork, new Score(value: 0, total: 10), compileMerge.text)
+            enviarReporte(fork, report)
             borrarArchivos(deletables)
             return
         }
 
-        // Ejecutamos la tarea mvn test
-        def testsExecution = "gradle -p ${canonicalCloneDir}".execute()
+        // Ejecutamos las pruebas
+        def testsExecution = ['docker', 'run', '--rm', '-v', (System.getProperty("user.dir") + '/' + canonicalCloneDir + ':/home/gradle/project'), '-w', '/home/gradle/project', 'gradle:alpine', 'gradle'].execute()
         testsExecution.waitFor()
 
-        // Tomamos el reporte y lo mostramos
         try {
 
             def junitParser = new JUnitParser()
             def junitIssues
 
-            def myTestsFile = new File("${canonicalCloneDir}/build/test-results")
+            def myTestsFile = new File("${canonicalCloneDir}/build/test-results/test")
+
             myTestsFile.eachFile(FileType.FILES) {
                 junitIssues = junitParser.parse(it)
                 // por el momento todos los tests se encuentran en el mismo archivo
                 // pero no sabemos el nombre exacto
             }
             def junitScore = new Score(value: junitIssues["tests"] - junitIssues["issues"].size(),
-                    total: junitIssues["tests"]).normalize(10)
+                    total: junitIssues["tests"]).normalize(8)
 
 
             def checkstyleParser = new CheckstyleParser()
             def myCheckstyleFile = new File("${canonicalCloneDir}/build/reports/checkstyle/main.xml")
             def checkstyleIssues = checkstyleParser.parse(myCheckstyleFile)
 
-            def checkstyleScore = new Score(value: Math.max(20 - checkstyleIssues.size(), 0), total: 20).normalize(10)
+            def checkstyleScore = new Score(value: Math.max(20 - checkstyleIssues.size(), 0), total: 20).normalize(2)
 
             def finalScore = junitScore + checkstyleScore
 
             def resultados = [
                     [
                             type  : 'junit',
-                            score : junitScore,
+                            score : junitScore.value,
                             issues: junitIssues.issues.collect { it.toMap() }
                     ],
                     [
                             type  : 'checkstyle',
-                            score : checkstyleScore,
+                            score : checkstyleScore.value,
                             issues: checkstyleIssues.collect { it.toMap() }
                     ]
 
             ]
 
-            def report = armarReporte(repoo, forko, finalScore, resultados)
-            enviarReporte(repoInDB, report)
-            borrarArchivos(deletables)
-
+            def report = armarReporte(fork, finalScore.normalize(10).value, resultados)
+            enviarReporte(fork, report)
         } catch (FileNotFoundException e) {
             log.info 'Se ha enviado una tarea que no corresponde con la interfaz provista'
-            def report = armarReporteFallido(repoo, forko, new Score(value: 0, total: 10), 'Se ha enviado una tarea que no corresponde con la interfaz provista')
-            enviarReporte(repoInDB, report)
-            borrarArchivos(deletables)
-            return
+            def report = armarReporteFallido(fork, new Score(value: 0, total: 10), 'Se ha enviado una tarea que no corresponde con la interfaz provista')
+            enviarReporte(fork, report)
         } catch (Exception e) {
             log.error("Falla la corrección!", e)
+        } finally {
             borrarArchivos(deletables)
         }
-
-        borrarArchivos(deletables)
-
     }
 
-    private armarReporteFallido(repoo, forko, finalScore, texto) {
+    private armarReporteFallido(fork, finalScore, texto) {
         def report = [
-                parent  : new RepoDTO(repo: repoo).toMap(),
-                fork    : new RepoDTO(repo: forko).toMap(),
                 test_run: [
                         score  : finalScore,
+                        sha: fork.sha,
                         details: texto
                 ]
         ]
         report
     }
 
-    private armarReporte(repoo, forko, finalScore, resultados) {
+    private armarReporte(fork, finalScore, resultados) {
         def report = [
-                parent  : new RepoDTO(repo: repoo).toMap(),
-                fork    : new RepoDTO(repo: forko).toMap(),
                 test_run: [
                         score  : finalScore,
+                        sha: fork.sha,
                         results: resultados
                 ]
         ]
         report
     }
 
-    private void enviarReporte(repoInDB, report) {
+    private void enviarReporte(fork, report) {
 
         def http = new HTTPBuilder(PropertiesHolder.instance.properties.DASHBOARD_HOST)
 
         try {
             http.request(Method.POST, ContentType.JSON) {
-                uri.path = repoInDB.full_name
+                uri.path = "/api/v1/repos/${fork.id}/grade"
                 headers.'x-api-key' = PropertiesHolder.instance.properties.GRADER_TOKEN
                 body = report
 
@@ -256,8 +212,8 @@ class GraderScript {
         } catch (HttpHostConnectException e) {
             log.error("Fallo en la conexión", e)
             log.info "Reintentando en 5 minutos..."
-            sleep(5 * 60_000)
-            enviarReporte(repoInDB, report)
+            sleep(5 * 60 * 1000)
+            enviarReporte(fork, report)
         }
     }
 
